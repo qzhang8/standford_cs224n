0d3a2a199def93078299c4bd1c0f3df71cd0a218
qi.zhang <qi.zhang@intel.com>
2023-09-26 09:30:59 +0800

    Q1.3

--- assignment1.py
+++ assignment1.py
@@ -81,3 +81,28 @@ def compute_co_occurrence_matrix(corpus, window_size=4):
     ### SOLUTION END
 
     return M, word2ind
+
+#Question 1.3    
+def reduce_to_k_dim(M, k=2):
+    """ Reduce a co-occurence count matrix of dimensionality (num_corpus_words, num_corpus_words)
+        to a matrix of dimensionality (num_corpus_words, k) using the following SVD function from Scikit-Learn:
+            - http://scikit-learn.org/stable/modules/generated/sklearn.decomposition.TruncatedSVD.html
+    
+        Params:
+            M (numpy matrix of shape (number of unique words in the corpus , number of unique words in the corpus)): co-occurence matrix of word counts
+            k (int): embedding size of each word after dimension reduction
+        Return:
+            M_reduced (numpy matrix of shape (number of corpus words, k)): matrix of k-dimensioal word embeddings.
+                    In terms of the SVD from math class, this actually returns U * S
+    """    
+    n_iters = 10     # Use this parameter in your call to `TruncatedSVD`
+    M_reduced = None
+    print("Running Truncated SVD over %i words..." % (M.shape[0]))
+    
+    ### SOLUTION BEGIN
+    svd = TruncatedSVD(n_components=k, n_iter=n_iters)
+    M_reduced = svd.fit_transform(M)
+    ### SOLUTION END
+
+    print("Done.")
+    return M_reduced
